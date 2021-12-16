028ad5960d8c89007413682c38143474967d739b
Domingos Gomes <domingos.gomes@xpand-it.com>
2021-12-16 16:52:48 +0000

    Initial commit

---
+++ src/com/company/Person.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    private String name;
+    private int age;
+    private String gender;
+
+    Person() {
+    };
+
+    Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Name:" + name + "\nAge: " + age + "\nGender: " + gender;
+    }
+}
