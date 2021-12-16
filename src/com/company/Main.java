a8868db0801702b8f5f75f90bb9036c24f96ee36
Domingos Gomes <domingos.gomes@xpand-it.com>
2021-12-16 17:29:59 +0000

    add congisadd new item

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,9 @@ public class Main {
     //Marco nao esteve aqui
     public static void main(String[] args) {
         Person p1 = new Person("John", 30, "Male");
-        Person p2 = new Person("Rachel", 25, "Female");
-        Person p3 = new Person("Mia",99,"Female");
+        Person p2 = new Person("21321312", 25, "Fem3212321ale");
+        Person p4 = new Person("12212121", 25, "Female");
+        Person p3 = new Person("Mia",99,"1221212");
         try {
             FileOutputStream f = new FileOutputStream(new File("myObjects.txt"));
             ObjectOutputStream o = new ObjectOutputStream(f);
