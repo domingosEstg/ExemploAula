0e9b3c259d60a71a6e0e9887328bc33828f4dee3
gonsalo-22 <goncalomaciel2250@gmail.com>
2021-12-16 17:04:18 +0000

    New person

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Person p1 = new Person("John", 30, "Male");
         Person p2 = new Person("Rachel", 25, "Female");
-        Person p3 = new Person("Mia",50,"Female");
+        Person p3 = new Person("Mia",100,"Female");
         try {
             FileOutputStream f = new FileOutputStream(new File("myObjects.txt"));
             ObjectOutputStream o = new ObjectOutputStream(f);
